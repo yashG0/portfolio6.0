5ecc507a08fae5afd7eec9003c80fb52ec99af20
Your Name <yashgaurkar0@gmail.com>
2023-09-14 17:31:10 +0530

    Your commit message

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
    <>
-    <div name='contact' className='w-full h-screen bg-slate-700 flex justify-center items-center p-4'>
+    <div name='contact' className='w-full sm:h-screen bg-slate-700 flex justify-center items-center p-4'>
       <form action="https://getform.io/f/e1c71e22-0753-4800-a681-2ffaeb6f3102" className='sm:mt-36 flex flex-col max-w-[600px] w-full' method='POST'>
           <div className='pb-8 '>
             <p className='text-4xl font-bold inline border-b-4 border-[#7CD795] text-gray-300'>Contact</p>
@@ -12,7 +12,7 @@ const Contact = () => {
           <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
           <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='name' />
           {/* <input type="email" placeholder='Email' name='name' /> */}
-          <textarea className='p-2 bg-[#ccd6f6] ' name="Message" id="" rows="10" placeholder='Your Message'></textarea>
+          <textarea className='p-2 bg-[#ccd6f6] ' name="Message" id="" rows="6" placeholder='Your Message'></textarea>
           <button className='text-white border-2 hover:bg-[#ccd6f6] hover:border-[#ccd6f6] px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
       </form>
 
