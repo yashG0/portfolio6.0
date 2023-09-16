c0a050c6c31b4828c61f3d84a39cc18e7dd38aae
Your Name <yashgaurkar0@gmail.com>
2023-09-16 17:26:32 +0530

    outline updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,13 +9,12 @@ const Contact = ({ dark }) => {
             <p className={`${dark ? 'text-gray-200' : 'text-gray-700'} text-4xl font-bold inline sm:text-center sm:block w-fit sm:mx-auto border-b-4 border-blue-500`}>Contact</p>
             <p className={`${dark ? 'text-gray-300' : 'text-gray-600'} py-4`}>// Submit the Form Below or shoot me an Email - myemail@gmail.com</p>
           </div>
-          <input className={`${dark ? 'bg-slate-300 text-slate-700' : 'bg-slate-700 text-slate-300'} p-2`} type="text" placeholder='Name' name='name' />
-          <input className={`${dark ? 'bg-slate-300 text-slate-700' : 'bg-slate-700 text-slate-300'} my-4 p-2 bg-[#ccd6f6]`} type="email" placeholder='Email' name='name' />
+          <input className={`outline-none ${dark ? 'bg-slate-300 text-slate-700' : 'bg-slate-700 text-slate-300'} p-2`} type="text" placeholder='Name' name='name' />
+          <input className={`outline-none ${dark ? 'bg-slate-300 text-slate-700' : 'bg-slate-700 text-slate-300'} my-4 p-2 bg-[#ccd6f6]`} type="email" placeholder='Email' name='name' />
           {/* <input type="email" placeholder='Email' name='name' /> */}
-          <textarea className={`${dark ? 'bg-slate-300 text-slate-700' : 'bg-slate-700 text-slate-300'} p-2 bg-[#ccd6f6]`} name="Message" id="" rows="6" placeholder='Your Message'></textarea>
+          <textarea className={`outline-none ${dark ? 'bg-slate-300 text-slate-700' : 'bg-slate-700 text-slate-300'} p-2 bg-[#ccd6f6]`} name="Message" id="" rows="6" placeholder='Your Message'></textarea>
           <button className={`${dark ? 'hover:bg-blue-500 border-blue-500' : 'border-blue-500 hover:bg-blue-500'} border-2 px-4 py-3 my-8 mx-auto flex items-center`}>Let's Collaborate</button>
         </form>
-
       </div>
     </>
   )
