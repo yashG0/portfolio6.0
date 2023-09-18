8e84fce2c2f662f084e43199fa9c03c4db1b59bc
yashG0 <yashgaurkar0@gmail.com>
2023-09-18 13:58:05 +0530

    new navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -91,13 +91,15 @@ const Navbar = ({ dark, setDark }) => {
 
     return (
         <>
-            <div className={`fixed w-full h-[80px] shadow-sm shadow-gray-300 flex justify-between items-center px-4  z-10 ${dark ? 'bg-gray-700 text-gray-200 shadow-gray-800' : 'shadow-gray-300 bg-gray-200 text-gray-700'}`}>
-                <div className={` text-2xl sm:text-4xl font-bold  cursor-pointer`}>
+            <div className={`fixed w-full h-[80px] shadow-sm flex justify-between items-center px-4 z-50 ${dark ? 'bg-gray-700 text-gray-200 shadow-gray-800' : 'shadow-gray-400 bg-gray-200 text-gray-700'}`}>
+                <div className={`text-2xl sm:text-4xl font-bold  cursor-pointer`}>
                     {/* <Image src={Logo} alt='logo' width={180} height={180} /> */}
                     <Link to='hero' duration={400} smooth={true} className={`bg-gradient-to-tr  text-transparent bg-clip-text ${dark ? 'from-gray-500 to-blue-500' : 'from-gray-700 to-blue-800'}`}>
                         My Portfolio
                     </Link>
                 </div>
+
+
 
                 {/* MENU */}
 
@@ -119,9 +121,9 @@ const Navbar = ({ dark, setDark }) => {
 
                 {/* HAMBURGER */}
                 <div className={`${dark ? 'text-gray-200' : 'text-gray-700'} md:hidden z-30 cursor-pointer `} onClick={handleClick}>
-                    <div className={`w-6 h-[2.5px] my-1 ${dark ? 'text-slate-800 bg-slate-200' : 'bg-slate-800 text-slate-200'}  transition-all duration-300 ${nav ? 'rotate-45 translate-y-1.5' : ''}`}></div>
-                    <div className={`w-6 h-[2.5px] my-1 ${dark ? 'text-slate-800 bg-slate-200' : 'bg-slate-800 text-slate-200'}  transition-all duration-300 ${nav ? 'opacity-0' : ''}`}></div>
-                    <div className={`w-6 h-[2.5px] my-1 ${dark ? 'text-slate-800 bg-slate-200' : 'bg-slate-800 text-slate-200'}  transition-all duration-300 ${nav ? '-rotate-45 -translate-y-2' : ''}`}></div>
+                    <div className={`w-6 h-[2.5px] my-1 ${dark ? 'text-slate-800 bg-slate-200' : 'bg-slate-800 text-slate-200'}  transition-all duration-100 ${nav ? 'rotate-45 translate-y-1.5' : ''}`}></div>
+                    <div className={`w-6 h-[2.5px] my-1 ${dark ? 'text-slate-800 bg-slate-200' : 'bg-slate-800 text-slate-200'}  transition-all duration-100 ${nav ? 'opacity-0' : ''}`}></div>
+                    <div className={`w-6 h-[2.5px] my-1 ${dark ? 'text-slate-800 bg-slate-200' : 'bg-slate-800 text-slate-200'}  transition-all duration-100 ${nav ? '-rotate-45 -translate-y-2' : ''}`}></div>
                 </div>
 
 
@@ -140,10 +142,10 @@ const Navbar = ({ dark, setDark }) => {
                     </ul>
                 </div>
             </div>
-            
+
             {/* MOBILE MENU */}
 
-            <ul className={`md:hidden ease-in-out fixed rounded-md duration-200 flex w-fit flex-col justify-center items-center h-fit ${nav ? 'top-20 right-10' : '-top-48 right-10 '} ${dark ? 'bg-gray-200 text-gray-700' : 'bg-gray-700 text-gray-200'}`}>
+            {/* <ul className={`md:hidden ease-in-out fixed rounded-md duration-200 flex w-fit flex-col justify-center items-center h-fit ${nav ? 'top-20 right-10' : '-top-48 right-10 '} ${dark ? 'bg-gray-200 text-gray-700' : 'bg-gray-700 text-gray-200'}`}>
                 {
                     links.map(({ id, name, to }) => (
                         <li className='my-2 text-xl' id={id}>
@@ -153,7 +155,21 @@ const Navbar = ({ dark, setDark }) => {
                         </li>
                     ))
                 }
+            </ul> */}
+            <ul className={`md:hidden z-10 fixed h-[67px] w-full  ${nav ? 'top-20 duration-200' : 'hidden'} ${dark ? 'text-gray-200 bg-gray-600' : 'bg-gray-300 text-gray-700'}`}>
+                <div className='grid text-center grid-cols-3 h-full justify-center items-center'>
+                    {
+                        links.map(({ id, name, to }) => (
+                            <li className='text-sm ' id={id}>
+                                <Link onClick={handleClick} to={to} smooth={true} duration={500} className='capitalize font-semibold'>
+                                    {name}
+                                </Link>
+                            </li>
+                        ))
+                    }
+                </div>
             </ul>
+
         </>
     );
 };
